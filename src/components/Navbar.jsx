6bdf45ba0b2ade4586e084d4e1de8c15cbe65377
kelvin <kelvin.oy16@gmail.com>
2025-03-12 12:19:01 +0000

    Fixed Bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,38 @@
 import { Link } from "react-router-dom";
 import "../styles/components/Navbar.css";
 import "../styles/components/Media Queries/Navbar-mq.css";
-import { FaUser } from "react-icons/fa";
-import { FaBars} from "react-icons/fa"
-
-
+import { FaUser, FaBars } from "react-icons/fa";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
+  const [isScroll, setIsScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScroll(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="navbar">
-      
+    <nav className={`navbar ${isScroll ? "on_scroll" : ""}`}>
       <div>
-        <FaBars
-          className="nav_icon" 
-          size={25}
-          color="white"
-        />
+        <FaBars className="nav_icon" size={25} color="white" />
       </div>
 
       <div className="logo">
-        <Link to='/'><img src="/images/Logos/LuxeDrive_logo.png" alt="Logo" /></Link>
+        <Link to="/">
+          <img src="/images/Logos/LuxeDrive_logo.png" alt="Logo" />
+        </Link>
       </div>
 
       <div className="user">
-        <FaUser
-          className="nav_icon"
-          size={25}
-          color="white" 
-        />
+        <FaUser className="nav_icon" size={25} color="white" />
       </div>
     </nav>
   );
