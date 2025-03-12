ecf0da41d11adf9a74b1805bb5db8ea227ada194
kelvin <kelvin.oy16@gmail.com>
2025-03-12 16:41:25 +0000

    Nav Menu

--- src/components/Navmenu.jsx
+++ src/components/Navmenu.jsx
@@ -12,6 +12,44 @@ import { FaBuilding } from 'react-icons/fa';
 import { FaAngleRight } from 'react-icons/fa';
 
 function Navmenu() {
+
+    const [isBenzOpen, setIsBenzOpen] = useState(true);
+    const [isPorscheOpen, setIsPorscheOpen] = useState(false);
+    const [isBentleyOpen, setIsBentleyOpen] = useState(false);
+    const [isAstonOpen, setIsAstonOpen] = useState(false);
+
+    const [isActive, setIsActive] = useState(null);
+
+    const handleClick = (tab) => {
+        if(tab === 'Porsche'){
+            setIsPorscheOpen(true);
+            setIsBenzOpen(false);
+            setIsBentleyOpen(false);
+            setIsAstonOpen(false);
+        }
+        else if(tab === 'Bentley'){
+            setIsBentleyOpen(true);
+            setIsBenzOpen(false);
+            setIsPorscheOpen(false);
+            setIsAstonOpen(false);
+        }
+        else if(tab === 'Aston'){
+            setIsAstonOpen(true);
+            setIsBenzOpen(false);
+            setIsPorscheOpen(false);
+            setIsBentleyOpen(false);
+        }
+        else {
+            setIsBenzOpen(true);
+            setIsPorscheOpen(false);
+            setIsBentleyOpen(false);
+            setIsAstonOpen(false);
+        }
+    }
+
+    const handleActiveTab = (tab
+
+
   return (
     <div className="nav_menu">
       <div className="nav_menu_inner">
@@ -29,37 +67,37 @@ function Navmenu() {
                 <div className="flex_left_content">
                     <div className="flc_brands">
                         <div className="brand_list">
-                            <div className="brand_name">
+                            <div className="brand_name" onClick={() => handleClick('')}>
                                 <p>Mercedes Benz</p>
                                 <FaAngleRight />
                             </div>
-                            <div className="brand_name">
+                            <div className="brand_name" onClick={() => handleClick('Porsche')}>
                                 <p>Porsche</p>
                                 <FaAngleRight />
                             </div>
-                            <div className="brand_name">
+                            <div className="brand_name" onClick={() => handleClick('Aston')}>
                                 <p>Aston Martin</p>
                                 <FaAngleRight />
                             </div>
-                            <div className="brand_name">
+                            <div className="brand_name" onClick={() => handleClick('Bentley')}>
                                 <p>Bentley</p>
                                 <FaAngleRight />
                             </div>
                             
                         </div>
                         <div className="brand_img_container">
-                            <div className="nav_brand_img">
+                            {isBenzOpen && <div className="nav_brand_img">
                                 <img src="/images/Nav Images/G63_nav.png" alt="" />
-                            </div>
-                            <div className="nav_brand_img porsche">
+                            </div>}
+                            {isPorscheOpen && <div className="nav_brand_img">
                                 <img src="/images/Nav Images/Porsche-nav.png" alt="" />
-                            </div>
-                            <div className="nav_brand_img">
+                            </div>}
+                            {isAstonOpen && <div className="nav_brand_img">
                                 <img src="/images/Nav Images/Aston-nav.png" alt="" />
-                            </div>
-                            <div className="nav_brand_img">
+                            </div>}
+                            {isBentleyOpen && <div className="nav_brand_img">
                                 <img src="/images/Nav Images/Bentley-nav.png" alt="" />
-                            </div>
+                            </div>}
                         </div>
                     </div>
                 </div>
