b9c07ffce26e430638d7eaa3d5a9008c7f48d91f
kelvin <kelvin.oy16@gmail.com>
2025-04-09 11:20:34 +0000

    Fixed NavMenu Bug

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -2,12 +2,30 @@ import '../styles/pages/Hero.css'
 import '../styles/pages/Media Queries/Hero-mq.css'
 import Navbar from '../components/Navbar'
 import { motion } from 'framer-motion'
-
+import { useEffect, useState } from 'react'
 function Hero() {
+
+  const [message, setMessage] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMessage(false)
+    }, 5000)
+
+    return () => clearTimeout(timer)
+  })
 
   return (
     <div className="hero">
         <Navbar />
+
+        {message && <motion.div className='message'
+        initial={{opacity: 0, y: -150}}
+        animate={{opacity: 1, y: 0}}
+        transition={{duration: 1, ease: "easeOut", delay: .3}}
+        >
+          <p>This website is a work in progress. All features might not be available.</p>
+        </motion.div>}
         <div className="hero_image"></div>
         <motion.div
         className='hero_text'
